6c4bb2726b8fd5c3192551789d1339642ef9d621
DelZeiT <maksimcvetkov1106@gmail.com>
2023-08-18 22:34:21 +0300

    TIC-004: добавил вывод сообщения о неправильном вводе команды.

--- WORK_commands/feedBack_command.py
+++ WORK_commands/feedBack_command.py
@@ -1,4 +1,5 @@
 from aiogram import types
+from content.TEXT_content import text_wrong_input
 
 # ID группы админов
 admin_group_id = -1001657428568
@@ -6,6 +7,7 @@ admin_group_id = -1001657428568
 
 # функция обратной связи
 async def feedback(message: types.Message, bot):
+
     msg = message.text   # сообщение пользователя
     words = msg.split(' ')   # список слов сообщения
 
@@ -18,3 +20,7 @@ async def feedback(message: types.Message, bot):
         user_tag = message.from_user.username
 
         await bot.send_message(chat_id=admin_group_id, text=f"@{user_tag}: {error_message}", parse_mode='HTML')
+    # если команда введена не правильно
+    else:
+        await message.answer(text=text_wrong_input)
+        
