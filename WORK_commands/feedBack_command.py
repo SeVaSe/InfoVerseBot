c3968bbc0a1c0888dd69e3a29237a0de73507aab
DelZeiT <maksimcvetkov1106@gmail.com>
2023-08-18 21:55:27 +0300

    TIC-004: изменил указание ID пользователя в сообщении в группу админов для обратной связи на тег пользователя.

--- WORK_commands/feedBack_command.py
+++ WORK_commands/feedBack_command.py
@@ -8,7 +8,13 @@ admin_group_id = -1001657428568
 async def feedback(message: types.Message, bot):
     msg = message.text   # сообщение пользователя
     words = msg.split(' ')   # список слов сообщения
+
     # тригер "ифи ошибка:"
     if words[0].lower() == 'ифи' and words[1].lower() == 'ошибка:':
-        error_message = ' '.join(words[2:])   #
-        await bot.send_message(chat_id=admin_group_id, text=f"{message.from_user.id}: {error_message}", parse_mode='HTML')
+
+        error_message = ' '.join(words[2:])   # соединяем слова сообщения без тригера
+
+        # тег пользователя
+        user_tag = message.from_user.username
+
+        await bot.send_message(chat_id=admin_group_id, text=f"@{user_tag}: {error_message}", parse_mode='HTML')
