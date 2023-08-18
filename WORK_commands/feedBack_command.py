be1388dc21c415f3aaaa3efcbdb64e0c7f43c028
DelZeiT <maksimcvetkov1106@gmail.com>
2023-08-18 21:34:27 +0300

    TIC-004: добавил функцию обратной связи.

--- WORK_commands/feedBack_command.py
+++ WORK_commands/feedBack_command.py
@@ -1,1 +1,14 @@
-"""ОБРАТНАЯ СВЯЗЬ"""
+from aiogram import types
+
+# ID группы админов
+admin_group_id = -1001657428568
+
+
+# функция обратной связи
+async def feedback(message: types.Message, bot):
+    msg = message.text   # сообщение пользователя
+    words = msg.split(' ')   # список слов сообщения
+    # тригер "ифи ошибка:"
+    if words[0].lower() == 'ифи' and words[1].lower() == 'ошибка:':
+        error_message = ' '.join(words[2:])   #
+        await bot.send_message(chat_id=admin_group_id, text=f"{message.from_user.id}: {error_message}", parse_mode='HTML')
--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 from aiogram import Bot, Dispatcher, executor, types
 from content.API_content import *
 from content.TEXT_content import *
-from WORK_commands import weather_command
-
-
+# from WORK_commands import weather_command
+from WORK_commands.feedBack_command import feedback
 
 # конфиг бота
 bot = Bot(API_bot)
@@ -22,17 +21,11 @@ async def start_bot(message: types.Message):
     await message.answer(text_start)
 
 
+# старт функции обратной связи
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def start_feedback(message: types.Message):
+    await feedback(message, bot)
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_start)
-
-
-
-
-
-
-
-
-
-
-
-
