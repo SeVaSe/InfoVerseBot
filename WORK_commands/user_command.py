31ba871143a9ca0ee82fc03033b7b4ffd8dc2c13
DelZeiT <maksimcvetkov1106@gmail.com>
2023-08-19 21:29:30 +0300

    TIC-003: добавил вывод списка команд, если пользователь вызывет команду "ифи команды".

--- WORK_commands/user_command.py
+++ WORK_commands/user_command.py
@@ -1,8 +1,14 @@
 """ПОЛЬЗОВАТЕЛЬСКТЕ КОМАНДЫ"""
 from aiogram import types
+from content.TEXT_content import text_help, text_wrong_input
 import random
 
 
 # кости игральные
 async def play_dice(message: types.Message, bot):
     await bot.send_dice(message.chat.id)
+
+
+# функция "ифи команды"
+async def command_help(message: types.Message, bot):
+    await message.reply(text_help)
