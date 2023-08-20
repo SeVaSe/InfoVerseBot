41c120bd57cbf4f630197b22b07332bba7b6e14f
sevase <kotaseva8@gmail.com>
2023-08-20 12:50:52 +0300

    TIK-007: исправил проверку на команды в мейн

--- WORK_commands/weather_command.py
+++ WORK_commands/weather_command.py
@@ -1,11 +1,14 @@
 """ПОГОДА"""
 import json
 import types
+
+import aiogram
 from aiogram import *
 from content.API_content import API_weather
 import requests
 import datetime
 import math
+from content.TEXT_content import *
 
 
 # создание погоды
@@ -26,63 +29,59 @@ class CreateWeather:
         self.city = city
         self.response = response
 
-    # ошибка
-    def call_error(self):
-        pass
-
-    # получение данных json
+    # получение данных json и создание погоды
     def create_data(self):
-        # РАСПАКОВКА ДАННЫХ ИЗ JSON
-        data = self.response.json()  # json данные
-        city = data['name']
-        weather = data['weather'][0]['main']
-        temp = data['main']['temp']
-        feels_like = data['main']['feels_like']
-        temp_min = data['main']['temp_min']
-        temp_max = data['main']['temp_max']
-        pressure = data['main']['pressure']
-        humidity = data['main']['humidity']
-        wind = data['wind']['speed']
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-        length_of_the_day = sunset_timestamp - sunrise_timestamp
+        try:
+            # РАСПАКОВКА ДАННЫХ ИЗ JSON
+            data = self.response.json()  # json данные
+            city = data['name']
+            weather = data['weather'][0]['main']
+            temp = data['main']['temp']
+            feels_like = data['main']['feels_like']
+            temp_min = data['main']['temp_min']
+            temp_max = data['main']['temp_max']
+            pressure = data['main']['pressure']
+            humidity = data['main']['humidity']
+            wind = data['wind']['speed']
+            sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+            sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+            length_of_the_day = sunset_timestamp - sunrise_timestamp
 
-        if weather in self.code_to_smile:
-            wd = self.code_to_smile[weather]
-        else:
-            wd = "Не пойму погоду, попробуй сам определить 😢"
+            if weather in self.code_to_smile:
+                wd = self.code_to_smile[weather]
+            else:
+                wd = "Не пойму погоду, попробуй сам определить 😢"
 
-        text_weather = f'''📅 Погода на {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} 
+            text_weather = f'''📅 Погода на {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} 
+            
+            
+        - Погода в городе: {city} 🏙
+        - Температура: {temp}°C {wd}
+        - Ощущается как: {feels_like}°C 🌡
+        - Ветер: {wind} м/с 💨
+        - Максимально возможная температура: {temp_max}°C 📈
+        - Минимально возможная температура: {temp_min}°C 📉
+        - Давление: {math.ceil(pressure/1.333)} мм.рт.ст 🧼
+        - Влажность: {humidity} % 💧
+        - Восход солнца: {sunrise_timestamp} 🌅
+        - Закат солнца: {sunset_timestamp} 🌄
+        - Продолжительность дня: {length_of_the_day} ⏱
         
         
-    🏙 Погода в городе: {city} 
-    
-    Температура: {temp}°C {wd}
-    
-    🌡 Ощущается как: {feels_like}°C 
-    
-    💨 Ветер: {wind} м/с 
-    
-    📈 Максимально возможная температура: {temp_max}°C 
-    
-    📉 Минимально возможная температура: {temp_min}°C 
-    
-    🧼 Давление: {math.ceil(pressure/1.333)} мм.рт.ст
-    
-    💧 Влажность: {humidity} %
-    
-    🌅 Восход солнца: {sunrise_timestamp} 
-    
-    🌄 Закат солнца: {sunset_timestamp} 
-    
-    ⏱ Продолжительность дня: {length_of_the_day} 
-    
-    
-❤️ Приятного времяпровождения! 
-        '''
+    ❤️ Приятного времяпровождения! 
+            '''
+
+            print(json.dumps(data, indent=4, ensure_ascii=False))
+            return text_weather
+        except KeyError:
+            return
 
 
-        return text_weather
+
+
+
+
+
 
 
 # функция погоды
@@ -93,16 +92,45 @@ async def weather_info(message: types.Message):
     command = com_word_mes[1].lower()  # команда
     city = com_word_mes[2]  # город
 
-    # GET-запрос
+    # GET-запросы
     response = requests.get(
         f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&units=metric&appid={API_weather}"
     )
 
-    if trigger == 'ифи' and command == 'погода':
+    if command.startswith('погода'):
         create_weather = CreateWeather(trigger, command, city, response)
         weather_data = create_weather.create_data()
         await message.answer(weather_data)
+    else:
+        await message.answer(text_wrong_input)
 
 
+# функция погоды на завтра
+async def weather_info_forecast(message: types.Message):
+    # обработка строки
+    com_word_mes = message.text.split(' ', 3)
+    trigger = com_word_mes[0].lower()  # тригер слово
+    command1 = com_word_mes[1].lower()  # команда
+    command2 = com_word_mes[2].lower()
+    city = com_word_mes[3]  # город
+
+    print(f"trigger: {trigger}")
+    print(f"command1: {command1}")
+    print(f"command2: {command2}")
+    print(f"city: {city}")
+
+    # GET-запросы
+    response = requests.get(
+        f"http://api.openweathermap.org/data/2.5/forecast?q={city}&lang=ru&units=metric&appid={API_weather}"
+    )
+    print(json.dumps(response.json(), indent=4, ensure_ascii=False))
+
+
+    if command1 == 'погода' and command2 == 'завтра':
+        create_weather = CreateWeather(trigger, command1, city, response)
+        weather_data = create_weather.create_data()
+        await message.answer(weather_data)
+    else:
+        await message.answer(text_wrong_input)
 
 
