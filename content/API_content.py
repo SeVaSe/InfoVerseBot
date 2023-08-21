648ab023e9e2163c3b1daa4552b56547801c617b
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-08-21 22:04:03 +0300

    Update API_content.py

--- content/API_content.py
+++ content/API_content.py
@@ -1,10 +1,10 @@
 """ХРАНЯТСЯ ВСЕ КЛЮЧИ И API"""
 
 """API блок"""
-API_bot = '6451625521:AAG4CeEwhGxT9M9aY3r4yOAH8W0JxdrMb_s'  # API - тг бота
+API_bot = 'API bots'  # API - тг бота
 API_translate = ''  # API - переводчик
-API_weather = 'd93c8221518f6af6c0f85a38610df4a7'  # API - погоды
+API_weather = 'API weather'  # API - погоды
 API_wiki = ''  # API - вики
-API_converter = '20ea5abb01049fb975c40914f5c55c17'
+API_converter = 'API convert'
 # ID группы админов
-admin_group_id = -1001657428568
+admin_group_id = "код группы"
